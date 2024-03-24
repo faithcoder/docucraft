df8a85709429304338f511cbc7a47e595f2783ed
Abdullah Al Arif <140299847+arif-xs@users.noreply.github.com>
2024-03-25 00:32:31 +0600

    header setup

---
+++ components/Logo.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <div className="lg:flex">
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="Protocol"
+          className="h-6 w-auto"
+          width={100}
+          height={24}
+        />
+      </Link>
+    </div>
+  );
+}
